a30ec5aa5ef8ca8e61948aa916ab602855576dd3
cichy202@gmail.com <cichy202@gmail.com@18d15458-41b7-5253-a1d5-bfe9f8ae581b>
2013-11-02 12:47:32 +0000

    Started implementing TravelGoodRestWS. Fixed for group number for BankService. Fixed NiceView client in tests.

--- trunk/TravelGoodTests/test/niceview/test/NiceViewTest.java
+++ trunk/TravelGoodTests/test/niceview/test/NiceViewTest.java
@@ -1,15 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package niceview.test;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -17,27 +8,10 @@ import static org.junit.Assert.*;
  */
 public class NiceViewTest {
     
-    public NiceViewTest() {
+    
+    @Test
+    public void test() {
+        
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
