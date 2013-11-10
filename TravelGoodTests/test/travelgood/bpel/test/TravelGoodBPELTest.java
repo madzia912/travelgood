8e462767c2983fb38d94e5f6ebebd837eaeac301
cichy202@gmail.com <cichy202@gmail.com@18d15458-41b7-5253-a1d5-bfe9f8ae581b>
2013-11-10 19:19:01 +0000

    Clean up.

--- TravelGoodTests/test/travelgood/bpel/test/TravelGoodBPELTest.java
+++ TravelGoodTests/test/travelgood/bpel/test/TravelGoodBPELTest.java
@@ -1,66 +1,9 @@
 package travelgood.bpel.test;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
-import travelgood.utils.DateUtils;
-
 /**
  *
  * @author Jacob
  */
 public class TravelGoodBPELTest {
 
-    public TravelGoodBPELTest() {
-    }
-    
-    @Test
-    public void testCreateItinerary() {
-        String bookingNumber = createItinerary("Cow");
-        
-        if(true){}
-    }
-    
-    /*
-    @Test
-    public void getFlights() {
-        GetFlightsRequest request = new GetFlightsRequest();
-
-        request.setFrom("ABC");
-        request.setTo("CBA");
-        request.setTakeOffDate(DateUtils.getXmlGregorianCalendar(2012, 10, 10));
-
-        GetFlightsResponse response = getFlights(request);
-
-        assertTrue(response.getFlights().getFlight().size() == 1);
-    }
-
-    @Test
-    public void getHotels() {
-        GetHotelsRequest request = new GetHotelsRequest();
-
-        request.setCity("Paris");
-        request.setArrivalDate(DateUtils.getXmlGregorianCalendar(2012, 10, 10));
-
-        GetHotelsResponse response = getHotels(request);
-
-        assertTrue(response.getHotels().getHotel().get(0).getId().equals("hotel1"));
-    }
-
-    private static GetFlightsResponse getFlights(dk.dtu.travelgood.GetFlightsRequest getFlightsRequestPart) {
-        dk.dtu.travelgood.TravelGoodService service = new dk.dtu.travelgood.TravelGoodService();
-        dk.dtu.travelgood.TravelGoodPortType port = service.getTravelGoodPortTypeBindingPort();
-        return port.getFlights(getFlightsRequestPart);
-    }
-
-    private static GetHotelsResponse getHotels(dk.dtu.travelgood.GetHotelsRequest getHotelsRequestPart) {
-        dk.dtu.travelgood.TravelGoodService service = new dk.dtu.travelgood.TravelGoodService();
-        dk.dtu.travelgood.TravelGoodPortType port = service.getTravelGoodPortTypeBindingPort();
-        return port.getHotels(getHotelsRequestPart);
-    }*/
-
-    private static String createItinerary(java.lang.String userId) {
-        dk.dtu.travelgood.TravelGoodRPCService service = new dk.dtu.travelgood.TravelGoodRPCService();
-        dk.dtu.travelgood.TravelGoodRPCPortType port = service.getTravelGoodRPCPort();
-        return port.createItinerary(userId);
-    }   
 }
